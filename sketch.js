54b83c33ee422185dd89c2534ac60c52e9f71bc1
Ondřej Sláma <ondra.slama1@gmail.com>
2018-06-05 16:16:38 +0200

    Html elements

--- sketch.js
+++ sketch.js
@@ -1,5 +1,25 @@
 function setup() {
-    createCanvas(600, 400);    
+    canvas1 = createCanvas(600, 400);
+    jumpButton = createButton("JUMP!");
+    jumpButton.mouseClicked(shakeBalls);
+    jumpSlider = createSlider(100, 1000, 250, 50);
+    createP("Drag ball radius");
+    dragRadSlider = createSlider(5, 100, 30, 1); 
+    createP("Click ball radius");
+    clickRadSlider = createSlider(5, 100, 50, 1); 
+    createP("Drag ball mass");
+    dragMassSlider = createSlider(5, 200, 30, 1); 
+    createP("Click ball mass");
+    clickMassSlider = createSlider(5, 200, 50, 1);
+    createP("Gravitation constant");
+    gravitySlider = createSlider(3, 100, 30, 1);
+    createP("Boundary elasticity");
+    bounceSlider = createSlider(0, 1, 0.75, 0.05);
+    createP("Impact elasticity");
+    impulseSlider = createSlider(0, 1, 0.5, 0.05);    
+    createP("Air resitance");
+    airSlider = createSlider(0, .2, 0.01, 0.01);
+
     frameRate(60);
 }
 
@@ -7,16 +27,32 @@ function setup() {
 let balls = [];
 
 // Parameters
-let radius = 30;
-let g = 30;
-let bounceDamp = 0.75;
-let airDamp = 0.00;
+let dragRadius;
+let dragMass;
+let clickRadius;
+let clickMass;
+
+let g;
+let bounceDamp;
+let impulseDamp;
+let airDamp;
+let jumpIntensity;
 
 
 function draw() {
+    dragRadius      = dragRadSlider.value();
+    dragMass        = dragMassSlider.value();
+    clickRadius     = clickRadSlider.value();    
+    clickMass       = clickMassSlider.value();
+    g               = gravitySlider.value();
+    bounceDamp      = bounceSlider.value();
+    impulseDamp     = impulseSlider.value();
+    airDamp         = airSlider.value();
+    jumpIntensity   = jumpSlider.value();
+
     background(150);
     fill(255);
-    ellipse(mouseX, mouseY, radius);
+    ellipse(mouseX, mouseY, dragRadius);
 
     // Loop through balls    
     for(let i = balls.length - 1; i >= 0; i--){       
@@ -25,24 +61,33 @@ function draw() {
         }         
         balls[i].bounce();
         balls[i].move();        
-        balls[i].show();
-        balls[i].resetForce();
-              
+        balls[i].show();              
         if(balls[i].lifecycle() > 250) balls.splice(i,1);
 
     }    
 }
 
+function shakeBalls(){    
+    for(let b of balls){
+        b.force.y += jumpIntensity;
+    }
+
+}
+
 function mouseClicked(){
-    if(balls.length < 500) createBall(mouseX, mouseY, 50);    
+    if (mouseX < width && mouseY < height){ 
+        if(balls.length < 500) createBall(mouseX, mouseY, clickRadius, clickMass);
+    } 
 }
 
-function mouseDragged(){    
-    if(balls.length < 500) createBall(mouseX, mouseY, 30);
+function mouseDragged(){   
+    if (mouseX < width && mouseY < height){ 
+        if(balls.length < 500) createBall(mouseX, mouseY, dragRadius, dragMass);
+    }
 }
 
-function createBall(_x, _y, _m){
-    let _b = new Circle(_x, _y, _m);
+function createBall(_x, _y, _r, _m = _r){
+    let _b = new Circle(_x, _y, _r, _m);
     let overlaps = 0;
 
     for (let ball of balls){
@@ -71,7 +116,7 @@ function sign(arg){
 }
 
 class Circle {
-    constructor (x, y, m, r = m) {
+    constructor (x, y, r, m = r) {
         this.pos = createVector(x, y);
         this.vel = createVector(random(-5, 5), random(-5, -2));
         this.acc = createVector(0, 0);
@@ -93,16 +138,14 @@ class Circle {
     }
 
     move() {
-        this.force.x += -sign(this.vel.x) * (this.vel.x * this.vel.x) * airDamp;
-        this.force.y += -sign(this.vel.y) * (this.vel.y * this.vel.y) * airDamp + (g/frameRate()) * this.mass;
-
         this.pos.add(this.vel);
         this.vel.add(this.acc);
         this.acc = p5.Vector.div(this.force, this.mass);
+        this.force.x = -sign(this.vel.x) * (this.vel.x * this.vel.x) * airDamp;
+        this.force.y = -sign(this.vel.y) * (this.vel.y * this.vel.y) * airDamp + (g/frameRate()) * this.mass;
     }
 
     bounce() {
-
         // Bottom
         if ((this.pos.y > height - this.rad/2) && (this.vel.y > 0)){
             this.vel.y = -this.vel.y * bounceDamp;
@@ -119,17 +162,40 @@ class Circle {
         let penetration = max(this.pos.y - (height - this.rad/2), 0);
         let correctionMag = max(penetration - slop*this.rad/2, 0) * percent;
 
-        this.pos.y -= correctionMag;
+        this.pos.y -= correctionMag;        
 
         // Right
         if ((this.pos.x > width - this.rad/2) && (this.vel.x > 0)){
             this.vel.x = -this.vel.x * bounceDamp;      
         }
 
+        // Position corection 
+        penetration = max(this.pos.x - (width - this.rad/2), 0);
+        correctionMag = max(penetration - slop*this.rad/2, 0) * percent;
+
+        this.pos.x -= correctionMag;
+
         // Left
         if ((this.pos.x < this.rad/2) && (this.vel.x < 0)){
             this.vel.x = -this.vel.x * bounceDamp; 
         }
+
+        // Position corection 
+        penetration = max(this.rad/2 - this.pos.x, 0);
+        correctionMag = max(penetration - slop*this.rad/2, 0) * percent;
+
+        this.pos.x += correctionMag;
+
+        // Top
+        if ((this.pos.y < this.rad/2) && (this.vel.y < 0)){
+            this.vel.y = -this.vel.y * bounceDamp;            
+        }
+
+        // Position corection 
+        penetration = max(this.rad/2 - this.pos.y, 0);
+        correctionMag = max(penetration - slop*this.rad/2, 0) * percent;
+
+        this.pos.y += correctionMag;
     }
 
     lifecycle(){
@@ -165,7 +231,7 @@ class Circle {
                 return;
             
             // Calculate restitution
-            let e = .5;
+            let e = impulseDamp;
             
             // Calculate impulse scalar
             let j = -(1 + e) * velAlongNormal;
@@ -177,27 +243,14 @@ class Circle {
             other.vel.add(p5.Vector.mult(impulse, other.invMass)); 
             
             // Position corection            
-            let percent = 0.5 // usually 20% to 80%
-            let slop = 0.05 // usually 0.01 to 0.1
+            let percent = 0.7; // usually 20% to 80%
+            let slop = 0.02; // usually 0.01 to 0.1
             let penetration = coverage/2 - d;
             let correctionMag = max(penetration - slop*coverage, 0) / (this.invMass + other.invMass) * percent;
             let correction = p5.Vector.mult(normal, correctionMag);
 
             this.pos.sub(p5.Vector.mult(correction, this.invMass));
             other.pos.add(p5.Vector.mult(correction, other.invMass));
-
-            // let momentum1 = p5.Vector.div(this.vel, this.mass);
-            // let momentum2 = p5.Vector.div(other.vel, other.mass);
-            
-            // let repulsionForce = p5.Vector.sub(this.pos, other.pos); 
-            // let forceMag = (coverage - d);
-            // constrain(forceMag, 0, 100);
-            // repulsionForce.setMag(forceMag);
-            // this.force.add(repulsionForce);   
         }        
-    }
-
-    resetForce(){
-        this.force.set(0, 0);
     }
 }
